5798c96ec251e81942ac50a846fbb7affcc00531
BhanuPrakash256 <bhanuprakash256sp@gmail.com>
2023-06-30 01:48:58 +0530

    All necessary dependencies are added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Api from './Api';
+// import Navs from './Navs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      <>
+<div class="jumbotron bg-dark text-white jumbotron-fluid ">
+  <div class="container">
+    <h1 class="display-4 text-center">Stock Tracker</h1>
+  </div>
+</div>
+
+<div className="container-fluid p-0 rounded-0">
+  <div className="card text-blue rounded-0" >
+    <div className="card-body">
+      <h2 className="card-title text-center">Stock Tracker</h2>
     </div>
-  );
+  </div>
+</div>
+
+
+      <div>
+          <Api symbol="AMZN" />
+        </div></>
+    );
+  }
 }
 
 export default App;
